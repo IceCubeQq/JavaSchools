53aadd1d9d7d6433c6952de52d61e5e5e40e5b28
IceCubeQq <icecubepq@gmail.com>
2025-12-15 00:24:11 +0500

    Delete src/main/java/org/school/analysis/infrastructure/telegram directory

--- src/main/java/org/school/analysis/infrastructure/telegram/handlers/CommandHandler.java
+++
@@ -1,8 +0,0 @@
-package org.school.analysis.infrastructure.telegram.handlers;
-
-import org.school.analysis.infrastructure.telegram.bot.SchoolTelegramBot;
-
-public interface CommandHandler {
-    void sendWelcome(Long chatId, SchoolTelegramBot bot);
-    void sendHelp(Long chatId, SchoolTelegramBot bot);
-}
--- src/main/java/org/school/analysis/infrastructure/telegram/handlers/QueryHandler.java
+++
@@ -1,8 +0,0 @@
-package org.school.analysis.infrastructure.telegram.handlers;
-
-import org.school.analysis.infrastructure.telegram.bot.SchoolTelegramBot;
-
-public interface QueryHandler {
-    void showMenu(Long chatId, SchoolTelegramBot bot);
-    void handleCallback(Long chatId, String callbackData, SchoolTelegramBot bot);
-}
